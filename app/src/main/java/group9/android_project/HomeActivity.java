42f3404de8498aa659de9e04831906755e162f2b
denFete <halling.7@hotmail.com>
2015-10-20 16:04:40 +0200

    All members | two activitys

---
+++ app/src/main/java/group9/android_project/HomeActivity.java
@@ -0,0 +1,15 @@
+package group9.android_project;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Fetes on 2015-10-20.
+ */
+public class HomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+    }
+
+}
--- app/src/main/java/group9/android_project/LoginActivity.java
+++ app/src/main/java/group9/android_project/LoginActivity.java
@@ -1,7 +1,12 @@
 package group9.android_project;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Fetes on 2015-10-20.
  */
@@ -11,5 +16,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        Button btn = (Button)findViewById(R.id.loginBtn);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+            }
+        });
     }
+
 }
